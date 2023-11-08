6057f1cb7210400725f68bc6f05d355304d316dd
war <crypto22h@gmail.com>
2023-11-08 08:47:00 +0000

    Add project files.

---
+++ map-editor/map-editor.cpp
@@ -0,0 +1,12 @@
+﻿// map-editor.cpp : Defines the entry point for the application.
+//
+
+#include "map-editor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
